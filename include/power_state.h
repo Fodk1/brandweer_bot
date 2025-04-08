797914018d4b8f39536d0fec95fd50f76769c425
saooms <saooms@hotmail.com>
2025-04-09 00:03:57 +0200

    sync

---
+++ include/power_state.h
@@ -0,0 +1,12 @@
+#ifndef POWER_STATE_H
+#define POWER_STATE_H
+
+/**
+ * @brief Start watchdog timer that reset the whole system if timedout
+ * 
+ * @param timeOut in milli seconds to maximum of 32767.
+ */
+void startWatchdog(uint16_t timeOut);
+void feedWatchdog();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 #ifdef CORE_CM7
-  #include <RPC.h>
-  
+    #include <RPC.h>
+    #include "power_state.h"
+
     void setup() {
         Serial.begin(115200);
         RPC.begin();
+        // for (size_t i = 0; i < 2; i++){
+        //     digitalWrite(LED_BUILTIN, 1);
+        //     delay(500);
+        //     digitalWrite(LED_BUILTIN, 0);
+        //     delay(500);
+        // }
+        // startWatchdog(3000);
     }
   
-    /*
-     * The loop() function of the M7 core only prints "M7: hello" every second and prints
-     * the objects calculated by the M4 core when they are availible
-     */
     void loop() {
-        Serial.println("M7: Hello");
-        while (RPC.available()) { // Get the objects from the M4 core
-            Serial.write(RPC.read()); 
-        }  
-        delay(1000);
+        // feedWatchdog(); 
+        // delay(500);
     }
-#endif              
+#endif
 
 #ifdef CORE_CM4    
     #include <RPC.h>
+    #include "power_state.h"
 
     extern "C" {
         #include "image_processing.h"
@@ -40,9 +42,15 @@
         {0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 0, 0},
         {0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0}
     };
+
+    void ISR(){
+
+    }
   
     void setup() {
         SerialRPC.begin();
+        startWatchdog(5000);
+        __WFI();
     }
   
     /*
@@ -50,13 +58,12 @@
      * the found objects are sent to the M7 core in string format.
      */
     void loop() {
-        AllPerceivedObjs allObjs = processImage(testImage); // Calculate targets from image
-        
-        char returnStr[676];
-        for (uint8_t i = 0; i < allObjs.objCount; i++) { // Print all found objects
-            sprintf(returnStr,"Obj %u: y = %f, x = %f, size = %u.", i, allObjs.objs[i].y, allObjs.objs[i].x, allObjs.objs[i].obj_size);
-            SerialRPC.println(returnStr); 
+        for (size_t i = 0; i < 5; i++){
+            digitalWrite(LED_BUILTIN, 1);
+            delay(200);
+            digitalWrite(LED_BUILTIN, 0);
+            delay(200);
         }
-        delay(3000);
+        feedWatchdog();
     }
 #endif
